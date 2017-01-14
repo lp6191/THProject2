8e362665397f93f36004641ab5d6b42d0a5565ad
lp6191 <lan.prijatelj@gmail.com>
2017-01-14 22:37:29 +0100

    meets expectations
    also tested in Chrome, Firefox and Edge

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 var numberOfStudents = $(".student-list li").length;
 var paginationLinkContainer = $("<div></div>");
-//check if the number of students is right;
+//check if the number of students is right.
 //console.log(numberOfStudents);
+
+//initial display of the page.
+createLinks(numberOfStudents);
+hideStudents(10);
+
 // hide all but 10 students on the list
-function hideStudents(firstToShow, lastToShow){
+function hideStudents(lastToShow){
   $(".student-list").children().hide();
-  $(".student-list").children().slice(firstToShow, lastToShow).show();
+  $(".student-list").children().slice(lastToShow - 10, lastToShow).show();
 }
 
 //create pagination links
@@ -16,9 +21,19 @@ function createLinks(numberOfLinks){
   for(var i = 0; i < (numberOfLinks / 10); i++){
     $(".pagination ul").append("<li></li>");
     $(".pagination li").last().append("<a></a>");
+    $(".pagination a").last().attr("href", "#");
     $(".pagination a").last().text(i+1);
   }
 }
 
-hideStudents(0, 10);
-createLinks(numberOfStudents);
+//change the displayed page
+$(".pagination a").click(function(){
+  //find out which page you must display
+  var page = $(this).text();
+  page = parseInt(page);
+  //console.log(page);
+
+  //hide appropriate students
+  hideStudents(page * 10);
+});
+//code above has been tested in Chrome, Edge and Firefox and worked in all three of them
