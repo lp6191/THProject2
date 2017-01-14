3d0e4b014c75564cabbb07bdcb9cacb2fc175f56
lp6191 <lan.prijatelj@gmail.com>
2017-01-14 20:05:12 +0100

    step 1 and 2 of meets expectations

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var numberOfStudents = $(".student-list li").length;
+var paginationLinkContainer = $("<div></div>");
+//check if the number of students is right;
+//console.log(numberOfStudents);
+// hide all but 10 students on the list
+function hideStudents(firstToShow, lastToShow){
+  $(".student-list").children().hide();
+  $(".student-list").children().slice(firstToShow, lastToShow).show();
+}
+
+//create pagination links
+function createLinks(numberOfLinks){
+  $(".student-list").after("<div></div>");
+  $(".student-list").next().addClass("pagination");
+  $(".pagination").append("<ul></ul>");
+  for(var i = 0; i < (numberOfLinks / 10); i++){
+    $(".pagination ul").append("<li></li>");
+    $(".pagination li").last().append("<a></a>");
+    $(".pagination a").last().text(i+1);
+  }
+}
+
+hideStudents(0, 10);
+createLinks(numberOfStudents);
